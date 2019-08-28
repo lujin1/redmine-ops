518ec8e5474453ee1e0f6237f316a57885a60fdb
xiaojin <33309882+xiaojin525@users.noreply.github.com>
2019-08-28 15:29:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-from flask import Flask,request
-from api.redmine import Reply_redmine
-import logging
 from api.job import *
 from flasgger import Swagger
 from flask_basicauth import BasicAuth
+import os
 
 app = Flask(__name__)
 app.register_blueprint(job_api)
@@ -12,7 +10,7 @@ app.register_blueprint(redmine_api)
 Swagger(app)
 
 app.config['BASIC_AUTH_USERNAME'] = 'admin'
-app.config['BASIC_AUTH_PASSWORD'] = '123456'
+app.config['BASIC_AUTH_PASSWORD'] = os.getenv('PASSWORD')
 
 app.config['BASIC_AUTH_FORCE'] = True  # 整个站点都验证
 
@@ -21,4 +19,3 @@ basic_auth = BasicAuth(app)
 if __name__ == '__main__':
     scheduler.start()
     app.run(host='0.0.0.0')
-
